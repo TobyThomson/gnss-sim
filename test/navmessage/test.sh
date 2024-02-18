38e982d3932369fbd13fe3fcb556c2ff727cab9a
Toby Thomson <me@tobythomson.co.uk>
2024-02-18 14:27:30 +0000

    ignore working now?

--- test/navmessage/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run the simulator
-echo "GENERATING SIMULATION DATA..."
-../../build/gnss-sim -o file-output.bin
-
-# Run GNSS-SDR
-echo "RUNNING GNSS-SDR..."
-gnss-sdr --config_file=navmessage.conf
